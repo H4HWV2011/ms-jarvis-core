84bea27725dc67828004ca64b628f8ee05e1c471
H4HWV2011 <you@example.com>
2025-08-03 10:43:26 -0400

    Debug: deeper logs for brain, mother, technical

--- backendlib/brain/index.js
+++ backendlib/brain/index.js
@@ -1,6 +1,7 @@
+console.log("[DEBUG] brain/index.js start");
 // Main entry point for Ms. Jarvis Brain
-console.log("[DEBUG] Entered backendlib/brain/index.js");
 const mother = require('./mother/mother');
+console.log("[DEBUG] mother loaded in brain/index.js");
 module.exports = {
   converse: mother.converse
 };
