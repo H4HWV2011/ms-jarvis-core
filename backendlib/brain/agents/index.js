0d04562a50cedffa2317371e10a549ed2b0478e1
H4HWV2011 <you@example.com>
2025-08-03 11:19:07 -0400

    Add back spiritual agent

--- backendlib/brain/agents/index.js
+++ backendlib/brain/agents/index.js
@@ -1,7 +1,6 @@
 module.exports = {
   technical: require('./technical')
-  // All others commented out for test
-  // spiritual: require('./spiritual'),
+  spiritual: require('./spiritual'),
   // emotional: require('./emotional'),
   // creative: require('./creative'),
   // mountainshares: require('./mountainshares')
