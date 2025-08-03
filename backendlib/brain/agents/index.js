d569e5f9dfb4938d9dea621035c7f815668705a7
H4HWV2011 <you@example.com>
2025-08-03 10:25:39 -0400

    Debug: Only technical agent enabled

--- backendlib/brain/agents/index.js
+++ backendlib/brain/agents/index.js
@@ -1,9 +1,10 @@
 module.exports = {
-  technical: require('./technical'),
-  spiritual: require('./spiritual'),
-  emotional: require('./emotional'),
-  creative: require('./creative'),
-  mountainshares: require('./mountainshares')
+  technical: require('./technical')
+  // All others commented out for test
+  // spiritual: require('./spiritual'),
+  // emotional: require('./emotional'),
+  // creative: require('./creative'),
+  // mountainshares: require('./mountainshares')
   // sepolia: require('./sepolia'),
   // arbitrum: require('./arbitrum')
 };
